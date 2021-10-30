cf6a3d84cbf6596c6b6cb2dd65a380062f5f1888
Ivan Kulikov <ivankulikov187@gmail.com>
2021-10-30 12:46:20 +0300

    Remove all apps and disable web and ftp server

--- src/app/powermeter/powermeter_setup.h
+++
@@ -1,6 +0,0 @@
-#ifndef _POWERMETER_SETUP_H
-    #define _POWERMETER_SETUP_H
-
-    void powermeter_setup_tile_setup( uint32_t tile_num );
-
-#endif // _POWERMETER_SETUP_H
--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,7 @@
 #include "hardware/hardware.h"
 #include "hardware/powermgm.h"
 
-#include "app/calc/calc_app.h"
-#include "app/FindPhone/FindPhone.h"
 #include "app/example_app/example_app.h"
-#include "app/gps_status/gps_status.h"
-#include "app/kodi_remote/kodi_remote_app.h"
-#include "app/osmand/osmand_app.h"
-#include "app/powermeter/powermeter_app.h"
-#include "app/osmmap/osmmap_app.h"
-#include "app/mail/mail_app.h"
-#include "app/sailing/sailing.h"
-#include "app/stopwatch/stopwatch_app.h"
-#include "app/wifimon/wifimon_app.h"
-#include "app/calendar/calendar.h"
-#include "app/weather/weather.h"
-#include "app/activity/activity.h"
-#include "app/fx_rates/fx_rates.h"
-#include "app/IRController/IRController.h"
-#include "app/alarm_clock/alarm_clock.h"
 
 #if defined( NATIVE_64BIT )
     /**
@@ -49,24 +32,24 @@ void setup() {
     /**
      * apps here
      */
-    osmmap_app_setup();
-    weather_app_setup();
-    stopwatch_app_setup();
-    alarm_clock_setup();
-    activity_app_setup();
-    calendar_app_setup();
-    mail_app_setup();
-    gps_status_setup();
-    IRController_setup();
-    sailing_setup();
-    osmand_app_setup();
-    fxrates_app_setup();
-    powermeter_app_setup();
+//    osmmap_app_setup();
+//    weather_app_setup();
+//    stopwatch_app_setup();
+//    alarm_clock_setup();
+//    activity_app_setup();
+//    calendar_app_setup();
+//    mail_app_setup();
+//    gps_status_setup();
+//    IRController_setup();
+//    sailing_setup();
+//    osmand_app_setup();
+//    fxrates_app_setup();
+//    powermeter_app_setup();
     example_app_setup();
-    FindPhone_setup();
-    wifimon_app_setup();
-    calc_app_setup();
-    kodi_remote_app_setup();
+//    FindPhone_setup();
+//    wifimon_app_setup();
+//    calc_app_setup();
+//    kodi_remote_app_setup();
     /**
      * post hardware setup
      */
